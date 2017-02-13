99f7d29e7a8839815c2877753e2cc024c9b43a4a
Emil 'Skeen' Madsen <sovende@gmail.com>
2017-02-14 00:35:03 +0100

    No verbose on tar

--- tools/emscripten.sh
+++ tools/emscripten.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Pull the Linux Portable
-curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L | tar -xzv
+curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L | tar -xz
