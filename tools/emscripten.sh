db1dac76f55736a460c9c5529d0c48824177569e
Emil 'Skeen' Madsen <sovende@gmail.com>
2017-02-14 00:50:04 +0100

    Hardcode emscripten.tar.gz

--- tools/emscripten.sh
+++ tools/emscripten.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest 
 # Pull the Linux Portable
-curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L | tar -xz
+#curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L | tar -xz
+curl -L https://github.com/Skeen/Emscripten-Travis/releases/download/v0.0.1/emsdk_portable.tar.gz | tar -xz
