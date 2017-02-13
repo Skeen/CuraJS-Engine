5e00c714efe9ec636b50febaebecba5fb39b1b43
Emil 'Skeen' Madsen <sovende@gmail.com>
2017-02-14 00:40:16 +0100

    Add garbage output for debugging

--- tools/emscripten.sh
+++ tools/emscripten.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest 
 # Pull the Linux Portable
 curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L | tar -xz
