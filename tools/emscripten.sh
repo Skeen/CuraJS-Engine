7dccaeba9869650fb4b7d3775184981c4bf7fdc0
Emil 'Skeen' Madsen <sovende@gmail.com>
2017-02-14 00:53:13 +0100

    Provide some output of the untarred folder

--- tools/emscripten.sh
+++ tools/emscripten.sh
@@ -3,3 +3,5 @@
 # Pull the Linux Portable
 #curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L | tar -xz
 curl -L https://github.com/Skeen/Emscripten-Travis/releases/download/v0.0.1/emsdk_portable.tar.gz | tar -xz
+
+ls emsdk_portable/
