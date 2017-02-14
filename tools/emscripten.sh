e49bab2ef46f3d8c7223cce115ead1d37073c1ff
Emil 'Skeen' Madsen <sovende@gmail.com>
2017-02-14 03:49:48 +0100

    New emscripten compiled.

--- tools/emscripten.sh
+++ tools/emscripten.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 
 # Pull the Linux Portable
 #curl -s https://api.github.com/repos/Skeen/Emscripten-Travis/releases/latest | grep "browser_download_url" | sed 's#.*: "\(.*\)"#\1#g' | xargs curl -L | tar -xz
-curl -L https://github.com/Skeen/Emscripten-Travis/releases/download/v0.0.1/emsdk_portable.tar.gz | tar -xz
+curl -L https://github.com/Skeen/Emscripten-Travis/releases/download/v0.0.2/emsdk_portable_trusty.tar.gz | tar xz
 
 # Active it, to update paths, etc.
 ./emsdk_portable/emsdk activate latest 
